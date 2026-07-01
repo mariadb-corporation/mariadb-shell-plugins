ef715ee98d4baabb49454e78216cf0771fcd54f4
Mike Zinner <mike@zinner.org>
2026-07-01 10:00:43 +0200

    Improved UUID functions and revert get_sequence_id() to try an existing call to UUID_TO_BIN() first.

--- mrs_plugin/db_schema/mysql_rest_service_metadata.msm.project/releases/versions/mysql_rest_service_metadata_4.1.6.sql
+++ mrs_plugin/db_schema/mysql_rest_service_metadata.msm.project/releases/versions/mysql_rest_service_metadata_4.1.6.sql
@@ -1145,44 +1145,40 @@ END%%
 -- -----------------------------------------------------------------------------
 
 DROP FUNCTION IF EXISTS `UUID_TO_BIN_SWAP`%%
-CREATE FUNCTION UUID_TO_BIN_SWAP(u CHAR(36))
-RETURNS BINARY(16)
-DETERMINISTIC
+CREATE FUNCTION UUID_TO_BIN_SWAP(_uuid CHAR(36)) RETURNS BINARY(16) SQL SECURITY INVOKER DETERMINISTIC NO SQL
 BEGIN
-    DECLARE h CHAR(32);
-
-    SET h = REPLACE(u, '-', '');
-
     RETURN UNHEX(CONCAT(
-        SUBSTR(h, 13, 4),  -- time_hi_and_version
-        SUBSTR(h,  9, 4),  -- time_mid
-        SUBSTR(h,  1, 8),  -- time_low
-        SUBSTR(h, 17, 4),  -- clock_seq
-        SUBSTR(h, 21, 12)  -- node
-    ));
+            SUBSTR(_uuid, 15, 4),
+            SUBSTR(_uuid, 10, 4),
+            SUBSTR(_uuid,  1, 8),
+            SUBSTR(_uuid, 20, 4),
+            SUBSTR(_uuid, 25) ));
 END%%
 
 DROP FUNCTION IF EXISTS `BIN_TO_UUID_SWAP`%%
-CREATE FUNCTION BIN_TO_UUID_SWAP(b BINARY(16))
-RETURNS CHAR(36)
-DETERMINISTIC
+CREATE FUNCTION BIN_TO_UUID_SWAP(_bin BINARY(16)) RETURNS CHAR(36) SQL SECURITY INVOKER DETERMINISTIC NO SQL
 BEGIN
-    DECLARE h CHAR(32);
-
-    SET h = HEX(b);
-
-    RETURN LOWER(CONCAT(
-        SUBSTR(h,  9, 8), '-', -- time_low
-        SUBSTR(h,  5, 4), '-', -- time_mid
-        SUBSTR(h,  1, 4), '-', -- time_hi_and_version
-        SUBSTR(h, 17, 4), '-', -- clock_seq
-        SUBSTR(h, 21, 12)      -- node
-    ));
+    RETURN LCASE(CONCAT_WS('-',
+            HEX(SUBSTR(_bin,  5, 4)),
+            HEX(SUBSTR(_bin,  3, 2)),
+            HEX(SUBSTR(_bin,  1, 2)),
+            HEX(SUBSTR(_bin,  9, 2)),
+            HEX(SUBSTR(_bin, 11))
+                 ));
 END%%
 
 DROP FUNCTION IF EXISTS `get_sequence_id`%%
 CREATE FUNCTION `get_sequence_id`() RETURNS BINARY(16) SQL SECURITY INVOKER NOT DETERMINISTIC NO SQL
-RETURN UUID_TO_BIN_SWAP(UUID())%%
+BEGIN
+    DECLARE result BINARY(16);
+    DECLARE CONTINUE HANDLER FOR SQLEXCEPTION
+    BEGIN
+        SET result = UUID_TO_BIN_SWAP(UUID());
+    END;
+
+    SET result = UUID_TO_BIN(UUID(), 1);
+    RETURN result;
+END%%
 
 DROP FUNCTION IF EXISTS `valid_request_path`%%
 CREATE FUNCTION `valid_request_path`(path VARCHAR(255))
