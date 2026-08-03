acfab744533c867765e88688b60beae335115693
Rene Ramirez <rene.ramirez@mariadb.com>
2026-08-03 17:30:03 -0600

    Removing plugins that are not related to the MariaDB solution

--- gui/backend/gui_plugin/debugger/scripts/__lib/sql_editor/_open_connection_validate_mysqlx.js
+++
@@ -1,3 +0,0 @@
-var lib = ws.tokens.lib
-lib.sql_editor.open_connection_validate_mysql.params["default_schema"] = lib.sql_editor.open_connection_validate_mysqlx.params["default_schema"]
-ws.execute(lib.sql_editor.open_connection_validate_mysql.file)
--- gui/backend/gui_plugin/debugger/scripts/unit/global/request_errors/no_token_single_user_mode.js
+++
@@ -1,5 +0,0 @@
-ws.connect();
-
-ws.validateLastResponse({
-    "exception": ws.matchRegexp("Handshake status 400 Provided token is wrong.*")
-})
